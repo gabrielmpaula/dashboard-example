93908ad11673cb7a7de49aea2c43df82220d253d
Gabriel Melo de Paula <gabrielmpaula@gmail.com>
2019-03-18 21:06:47 +0800

    Add files via upload

---
+++ utils/dataframe.py
@@ -0,0 +1,16 @@
+import glob
+import os
+import pandas as pd
+
+def load_data():
+    data_dir = 'data/' 
+    file_extension = '*.csv'
+
+    all_files = glob.glob(os.path.join(data_dir, file_extension))
+    
+    df = []
+    
+    for filename in all_files:
+        df.append(pd.read_csv(filename))
+    
+    return df
